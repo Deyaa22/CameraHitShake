d816b43e0011c20a6314ecead75f89d07a810ad8
Deyaa Omer <61560614+Deyaa22@users.noreply.github.com>
2023-04-25 12:17:19 +0200

    Fix end of lerping process

--- CameraHitShake.cs
+++ CameraHitShake.cs
@@ -3,36 +3,28 @@ using UnityEngine;
 
 public class CameraHitShake : MonoBehaviour {
 
-    // If you like to improve it, Or create a pull request for it. 
-    // public enum DampingStyle { Lerp, Linear, Smooth }
-    // public DampingStyle m_DampingStyle = DampingStyle.Lerp;
-
     [HideInInspector] public Vector3 originalPos;
     [HideInInspector] public Quaternion originalRot;
     private Coroutine currentShake = null;
     public bool isShaking { get { return currentShake != null; } }
 
     // Transform of the camera to make Hit Shake.
-    [Header ("Camera Transform")]
-    [Tooltip ("Assign local transform automatically on Awake")]
+    [Header("Camera Transform")]
+    [Tooltip("Assign local transform automatically on Awake")]
     [SerializeField] private Transform camTransform;
 
     // The Power of the Hit Shake.
-    [Header ("Hit Shake Power")]
+    [Header("Hit Shake Power")]
     public float powerMultiplier = 1f;
-    [SerializeField][Range (0f, 50f)] private float _PositionShakePower = 2f;
-    [SerializeField][Range (0f, 360f)] private float _RotationShakePower = 15f;
+    [SerializeField][Range(0f, 50f)] private float _PositionShakePower = 2f;
+    [SerializeField][Range(0f, 360f)] private float _RotationShakePower = 15f;
 
     // Speed of camera to get back to original Poition and Rotation.
-    [Header ("Damping Speed")]
-    [SerializeField][Range (0f, 10f)] private float _PositionDampingSpeed = 10;
-    [SerializeField][Range (0f, 10f)] private float _RotationDampingSpeed = 10;
+    [Header("Damping Speed")]
+    [SerializeField][Range(0f, 10f)] private float _PositionDampingSpeed = 0.1f;
+    [SerializeField][Range(0f, 10f)] private float _RotationDampingSpeed = 0.1f;
 
-    // Minimum distance and orientation that not effect if changed.
-    private float minDistance = 0.05f;
-    private float minOrientation = 0.5f;
-
-    private void Awake () {
+    private void Awake() {
         if (camTransform == null)
             camTransform = this.transform;
 
@@ -44,39 +36,43 @@ public class CameraHitShake : MonoBehaviour {
     /// Play the Hit Shake of Camera transform.
     /// </summary>
     /// <param name="forced">Stop previous Hit Shake and start new one.</param>
-    public void Play (bool forced = true) {
+    public void Play(bool forced = true) {
         if (!camTransform)
             return;
 
-        if (isShaking) {
+        if (isShaking)
+        {
             if (!forced)
                 return;
             else
-                StopCoroutine (currentShake);
-        } else {
+                StopCoroutine(currentShake);
+        } else
+        {
             originalPos = camTransform.localPosition;
             originalRot = camTransform.localRotation;
         }
-        currentShake = StartCoroutine (Shake ());
+        currentShake = StartCoroutine(Shake());
     }
-    private IEnumerator Shake () {
+    private IEnumerator Shake() {
         camTransform.localPosition = originalPos + Random.insideUnitSphere * _PositionShakePower;
-        camTransform.localRotation = originalRot * Quaternion.Euler (Random.insideUnitSphere * _RotationShakePower);
-        while (camTransform.localPosition != originalPos || camTransform.localRotation != originalRot) {
-            camTransform.localPosition = Vector3.Lerp (camTransform.localPosition, originalPos, _PositionDampingSpeed * powerMultiplier * Time.deltaTime);
-            camTransform.localRotation = Quaternion.Slerp (camTransform.localRotation, originalRot, _RotationDampingSpeed * powerMultiplier * Time.deltaTime);
-            if (Vector3.Distance (camTransform.localPosition, originalPos) <= minDistance && Quaternion.Angle (camTransform.localRotation, originalRot) <= minOrientation) {
-                camTransform.localPosition = originalPos;
-                camTransform.localRotation = originalRot;
+        camTransform.localRotation = originalRot * Quaternion.Euler(Random.insideUnitSphere * _RotationShakePower);
+        float time = 0f;
+        while (camTransform.localPosition != originalPos || camTransform.localRotation != originalRot)
+        {
+            time += _PositionDampingSpeed * powerMultiplier * Time.deltaTime;
+            camTransform.localPosition = Vector3.Lerp(camTransform.localPosition, originalPos, time);
+            time += _RotationDampingSpeed * powerMultiplier * Time.deltaTime;
+            camTransform.localRotation = Quaternion.Slerp(camTransform.localRotation, originalRot, time);
+            if (camTransform.localPosition == originalPos && camTransform.localRotation == originalRot)
                 break;
-            }
             yield return null;
         }
         currentShake = null;
     }
-    public void Stop () {
-        if (currentShake != null) {
-            StopCoroutine (currentShake);
+    public void Stop() {
+        if (currentShake != null)
+        {
+            StopCoroutine(currentShake);
             currentShake = null;
         }
         camTransform.localPosition = originalPos;
